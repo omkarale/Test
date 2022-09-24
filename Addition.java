125ea2f5dae8d9527afdcdfb7454e067857664d4
omkarale <omkarale13@gmail.com>
2022-09-24 14:34:36 +0530

    -

---
+++ Addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+	public class Addition{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		long a;
+
+		System.out.println("Enter a number : ");
+		
+		a = sc.nextInt();
+		long b;
+		System.out.println("Enter a  2 number : ");
+		
+		b = sc.nextInt();
+				System.out.println("Enter a  2 number : ");
+		
+		long c = a+b;
+		
+		System.out.println("Addition Will BE: " +c);
+	}
+}
